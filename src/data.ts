a61383640d4088d45a06ab8dd3d39d7431c26e46
muindocheck <muin@docheck.id>
2023-02-09 10:39:02 +0700

    feat: add option id

--- src/data.ts
+++ src/data.ts
@@ -3,78 +3,78 @@ export const surveyQuestions = [
     id: 1,
     question: "How often do you visit the gym in a week?",
     options: [
-      { option: "Never" },
-      { option: "1-2 times" },
-      { option: "3 or more times" },
+      { id: 1, option: "Never" },
+      { id: 2, option: "1-2 times" },
+      { id: 3, option: "3 or more times" },
     ],
   },
   {
     id: 2,
     question: "What type of exercise do you prefer at the gym?",
     options: [
-      { option: "Cardiovascular" },
-      { option: "Strength training" },
-      { option: "Yoga or Pilates" },
+      { id: 1, option: "Cardiovascular" },
+      { id: 2, option: "Strength training" },
+      { id: 3, option: "Yoga or Pilates" },
     ],
   },
   {
     id: 3,
     question: "What time of day do you usually visit the gym?",
     options: [
-      { option: "Morning" },
-      { option: "Afternoon" },
-      { option: "Evening" },
+      { id: 1, option: "Morning" },
+      { id: 2, option: "Afternoon" },
+      { id: 3, option: "Evening" },
     ],
   },
   {
     id: 4,
     question: "How important is the gym's location to you?",
     options: [
-      { option: "Very important" },
-      { option: "Somewhat important" },
-      { option: "Not important" },
+      { id: 1, option: "Very important" },
+      { id: 2, option: "Somewhat important" },
+      { id: 3, option: "Not important" },
     ],
   },
   {
     id: 5,
     question: "Do you prefer working out alone or with a partner or group?",
     options: [
-      { option: "Alone" },
-      { option: "With a partner" },
-      { option: "In a group" },
+      { id: 1, option: "Alone" },
+      { id: 2, option: "With a partner" },
+      { id: 3, option: "In a group" },
     ],
   },
   {
     id: 6,
     question: "How satisfied are you with the gym's equipment and facilities?",
     options: [
-      { option: "Very satisfied" },
-      { option: "Somewhat satisfied" },
-      { option: "Not satisfied" },
+      { id: 1, option: "Very satisfied" },
+      { id: 2, option: "Somewhat satisfied" },
+      { id: 3, option: "Not satisfied" },
     ],
   },
   {
     id: 7,
     question: "Have you taken any gym classes or personal training sessions?",
     options: [
-      { option: "Yes" },
-      { option: "No" },
-      { option: "I'm interested but haven't yet" },
+      { id: 1, option: "Yes" },
+      { id: 2, option: "No" },
+      { id: 3, option: "I'm interested but haven't yet" },
     ],
   },
   {
     id: 8,
     question: "How would you rate the customer service at the gym?",
-    options: [{ option: "Excellent" }, { option: "Good" }, { option: "Poor" }],
+    options: [{ id: 1, option: "Excellent" }, { id: 2, option: "Good" }, { id: 3, option: "Poor" }],
   },
   {
     id: 9,
     question:
       "Do you think the gym should offer more variety in its exercise classes?",
     options: [
-      { option: "Yes, definitely" },
-      { option: "Maybe, I'm not sure" },
-      { option: "No, I'm happy with the current offerings" },
+      { id: 1, option: "Yes, definitely" },
+      { id: 2, option: "Maybe, I'm not sure" },
+      { id: 3, option: "No, I'm happy with the current offerings" },
     ],
   },
   {
@@ -82,9 +82,9 @@ export const surveyQuestions = [
     question:
       "How likely are you to recommend the gym to a friend or family member?",
     options: [
-      { option: "Highly likely" },
-      { option: "Somewhat likely" },
-      { option: "Not likely" },
+      { id: 1, option: "Highly likely" },
+      { id: 2, option: "Somewhat likely" },
+      { id: 3, option: "Not likely" },
     ],
   },
 ];
