b18da22e15fb310e236056013dfd0074c54d50bb
muindocheck <muin@docheck.id>
2023-02-09 17:03:55 +0700

    feat: add review component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,5 @@
 // Library
-import { useCallback, useEffect, useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 
 // Component
 import Button from "../Button/Button";
@@ -11,6 +11,7 @@ import { SurveyQuestion, surveyQuestions } from "../../data";
 // Helper
 import { removeFromLocalStorage, saveToLocalStorage } from "../../helper";
 import Timer from "../Timer/Timer";
+import Review from "./Review";
 
 function Form() {
   const [questions, setQuestions] = useState<SurveyQuestion[]>(surveyQuestions);
@@ -19,14 +20,15 @@ function Form() {
   );
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const [page, setPage] = useState<number>(1);
-  const [timer, setTimer] = useState<number>(180);
+  const [timer, setTimer] = useState<number>(60);
+  const [isForm, setIsForm] = useState<boolean>(true);
 
-  function handleSubmit() {
+  const handleSubmit = () => {
     if (selectedOption === null) return alert("Please select an option");
 
     if (page === surveyQuestions.length) {
       alert("Form submitted!");
-      removeFromLocalStorage();
+      setIsForm(false);
       setPage(1);
     } else {
       saveToLocalStorage(selectedOption, activeQuestion);
@@ -34,55 +36,58 @@ function Form() {
       setActiveQuestion(questions[page]);
       setSelectedOption(null);
     }
-    console.log("submit");
-  }
+  };
 
-  function handleReset() {
-    setPage(1);
+  const handleReset = () => {
+    setIsForm(true);
+    setPage(1)
     setActiveQuestion(questions[0]);
-    setSelectedOption(null);
-    setTimer(180);
-    console.log("reset");
-  }
+    removeFromLocalStorage();
+  };
 
-  function handleOptionChange(e: React.ChangeEvent<HTMLInputElement>) {
+  const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedOption(e.target.value);
-    console.log("change");
-  }
+  };
 
- const handleTimer = useCallback(() => {
+  const handleTimer = useCallback(() => {
     alert("Time is up!");
-    setPage(questions.length);
-    setActiveQuestion(questions[questions.length - 1]);
-    setSelectedOption(null);
-    setTimer(5)
-    console.log("timer");
-  }, [timer]);
+    setIsForm(false);
+  }, []);
 
   return (
-    <section className="h-screen w-auto sm:max-w-md grid mx-auto content-center p-8 md:p-0 shadow-sm">
-      <Timer timer={timer} handleTimer={handleTimer} />
-      <form className="rounded-2xl mb-5 bg-white py-5 px-10">
-        <h1 className="mb-4 font-semibold text-2xl text-slate-300">Q{page}</h1>
-        <p className="mb-4 font-semibold text-2xl text-purple-700">
-          {activeQuestion.question}
-        </p>
-        {activeQuestion.options.map(({ option, id }, index) => (
-          <RadioInput
-            selectedOption={selectedOption}
-            onChange={handleOptionChange}
-            id={id}
-            option={`${activeQuestion.id}`}
-            label={option}
-            key={index}
-          />
-        ))}
+    <section className="w-auto sm:max-w-md py-8">
+      {isForm ? <Timer timer={timer} handleTimer={handleTimer} /> : null}
+      <form className="rounded-2xl mb-5 bg-white py-5 px-10 shadow-sm">
+        {isForm ? (
+          <Fragment>
+            <h1 className="mb-4 font-semibold text-2xl text-slate-300">
+              Q{page}
+            </h1>
+            <p className="mb-4 font-semibold text-2xl text-purple-700">
+              {activeQuestion.question}
+            </p>{" "}
+            {activeQuestion.options.map(({ option, id }, index) => (
+              <RadioInput
+                selectedOption={selectedOption}
+                onChange={handleOptionChange}
+                id={id}
+                option={`${activeQuestion.id}`}
+                label={option}
+                key={index}
+              />
+            ))}
+          </Fragment>
+        ) : (
+          <Review />
+        )}
       </form>
-      <Button disabled={page === 1} text="Reset" onClick={handleReset} />
-      <Button
-        text={page === surveyQuestions.length ? "Submit" : "Next"}
-        onClick={handleSubmit}
-      />
+      {!isForm ? <Button text="Reset" onClick={handleReset} /> : null}
+      {isForm ? (
+        <Button
+          text={page === surveyQuestions.length ? "Submit" : "Next"}
+          onClick={handleSubmit}
+        />
+      ) : null}
     </section>
   );
 }
---
+++ src/components/Form/Review.tsx
@@ -0,0 +1,31 @@
+// Library
+import { Fragment, useEffect, useState } from "react";
+
+function Review() {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("surveyData") || "[]");
+    if (data) {
+      setData(data);
+    }
+  }, []);
+
+  return (
+    <Fragment>
+      {" "}
+      <h1 className="mb-4 font-semibold text-2xl text-center text-purple-700">
+        Thanks you for finishing the surveys!
+      </h1>
+      <p className="text-slate-700 mb-2">Survey Results:</p>
+      {data ? data.map((item: any) => (
+        <div className="mb-3" key={item.id}>
+          <p className="font-semibold text-sm text-purple-700 mb-1">{item.question}</p>
+          <p className="text-sm text-purple-600">- {item.answer}</p>
+        </div>
+      )) : <p>No Data</p>}
+    </Fragment>
+  );
+}
+
+export default Review;
